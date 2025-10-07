aaa43712a5de2568b0d48572e6099fff30661a86
hroxo <hugo.roxo04@gmail.com>
2025-10-07 22:45:43 +0100

    fixed

--- ex/ex19/ft_sort_params.c
+++ ex/ex19/ft_sort_params.c
@@ -1,45 +1,74 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hroxo <hroxo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/07 19:47:23 by hroxo             #+#    #+#             */
-/*   Updated: 2025/10/07 22:09:01 by hroxo            ###   ########.fr       */
+/*   Created: 2025/10/07 22:22:10 by hroxo             #+#    #+#             */
+/*   Updated: 2025/10/07 22:40:55 by hroxo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-void	ft_putstr(char	*str)
+void	ft_putargs(char	**str, int n)
 {
 	int	i;
+	int	j;
 
-	i = 0;
-	while (str[i])
+	i = 1;
+	while (i < n)
 	{
-		ft_putchar(str[i]);
+		j = 0;
+		while (str[i][j])
+		{
+			ft_putchar(str[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
 		i++;
 	}
 }
 
-int	main(int argc, char **argv)
+int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*tmp;
 
 	if (argc < 2)
 		return (1);
 	i = 1;
 	while (i < argc)
 	{
-		ft_putstr(argv[i]);
-		ft_putchar('\n');
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				tmp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = tmp;
+			}
+			j++;
+		}
 		i++;
 	}
+	i = 1;
+	ft_putargs(argv, argc);
 	return (0);
 }
-
 /*
 #include <unistd.h>
 

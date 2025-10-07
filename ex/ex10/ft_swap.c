e1de34146641fef66879fa5e05121839e4f02854
Hugo Roxo <hroxo@c1r15s9.42porto.com>
2025-10-07 13:32:18 +0100

    norminetted

--- ex/ex10/ft_swap.c
+++ ex/ex10/ft_swap.c
@@ -10,9 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_swap(int *a, int *b)
+void	ft_swap(int *a, int *b)
 {
-	int temp;
+	int	temp;
 
 	temp = *a;
 	*a = *b;
